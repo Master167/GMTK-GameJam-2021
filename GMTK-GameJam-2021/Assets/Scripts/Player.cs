251c4683857fd2e4b057cd3a5c3755428f0c637a
Michael Frederick <master16789@gmail.com>
2021-06-11 14:28:31 -0500

    Create Player script
    Just a basic moving of the player

---
+++ GMTK-GameJam-2021/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool IsLogging;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player has started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        var direction = new Vector2(x, y);
+
+        Move(direction);
+    }
+
+    void Move(Vector2 direction)
+    {
+        //Find the screen limits to the player's movement
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        //Get the player's current position
+        Vector2 pos = transform.position;
+        //Calculate the proposed position
+        pos += direction * speed * Time.deltaTime;
+        //Ensure that the proposed position isn't outside of the limits
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+        //Update the player's position
+        transform.position = pos;
+    }
+
+}
